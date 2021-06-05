154af9854ef3e2b17065059ec223913b9ec7ecf7
mayfreq <osmankalkan001@gmail.com>
2021-06-05 10:58:12 +0300

    [quiz] initial commit

---
+++ quiz/src/App.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<template>
+  <h1>Quiz</h1>
+</template>
+
+<style>
+</style>
